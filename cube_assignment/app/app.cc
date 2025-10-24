03d90db28ee7624cf04d43ab85795f6200a00ad9
SeUz75 <seuz752@gmail.com>
2025-10-24 17:47:15 +0300

    Generated FIRST VIDEO git status !

--- cube_assignment/app/app.cc
+++ cube_assignment/app/app.cc
@@ -142,28 +142,30 @@ int main (int argc, char* argv[]) {
             break;
         }
     }
-
+    
     if ( !((w == 1280 && h == 720) || (w == 1632 && h == 1224) || (w == 1920 && h == 1080)) ) {
         std::cerr << "Please pass one of these pairs of resolutions " <<std::endl << "1280 x 720" << std::endl <<
-            "1632 x 1224" << std::endl << "1920 x 1080" << std::endl;
-            return -1;
+        "1632 x 1224" << std::endl << "1920 x 1080" << std::endl;
+        return -1;
     }
     std::cout << (int)y << std::endl;
     std::cout << (int)u << std::endl;
     std::cout << (int)v << std::endl;
+    // Minimal Y4M header must include
+    // YUV4MPEG2 W<width> H<height> F<fps_num>:<fps_den> Ip A0:0 C<chroma>
     std::ofstream video_buffer(output_file, std::ios::out | std::ios::binary);
-    video_buffer << "YUV4MPEG2 W" << w << " H" << h << " F" << f << ":1 Ip A0:0 C420jpeg\n";
+    video_buffer << "YUV4MPEG2 W" << w << " H" << h << " F" << f << ":30 Ip A0:0 C420jpeg\n";
 
     std::vector<uint8_t> y_background(w*h, y);
-    std::vector<uint8_t> u_background((w*h)/2, u);
-    std::vector<uint8_t> v_background((w*h)/2, v);
+    std::vector<uint8_t> u_background((w*h)/4, u);
+    std::vector<uint8_t> v_background((w*h)/4, v);
 
     for (int i = 0; i < f; i++) {
         video_buffer << "FRAME\n";
 
         video_buffer.write(reinterpret_cast<const char*>(y_background.data()), w*h);
-        video_buffer.write(reinterpret_cast<const char*>(u_background.data()), (w*h)/2);
-        video_buffer.write(reinterpret_cast<const char*>(v_background.data()), (w*h)/2);
+        video_buffer.write(reinterpret_cast<const char*>(u_background.data()), (w*h)/4);
+        video_buffer.write(reinterpret_cast<const char*>(v_background.data()), (w*h)/4);
     }
     return 0;
 }
